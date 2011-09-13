92febed58a4cf4381e3c39edca8aa93f7da15633
Lucas Maxwell <lucas.maxwell@c3businesssolutions.com.au>
2011-09-13 10:01:42 +1000

    Remove adapter_spec, isn't needed

--- lib/arjdbc/teradata/connection_methods.rb
+++ lib/arjdbc/teradata/connection_methods.rb
@@ -4,7 +4,6 @@ class ActiveRecord::Base
       config[:port] ||= 1025
       config[:url] ||= "jdbc:teradata://#{config[:host]}/DATABASE=#{config[:database]},DBS_PORT=#{config[:port]},COP=OFF"
       config[:driver] ||= "com.teradata.jdbc.TeraDriver"
-      config[:adapter_spec] = ::ArJdbc::Teradata
       jdbc_connection(config)
     end
   end
