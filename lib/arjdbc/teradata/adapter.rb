73082ed51b1573d54df7835dce0f0b65f08df9c7
Lucas Maxwell <lucas@thecowsays.mu>
2013-05-21 09:56:19 +1000

    Move teradata limit helpers to separate file

--- lib/arjdbc/teradata/adapter.rb
+++ lib/arjdbc/teradata/adapter.rb
@@ -1,6 +1,4 @@
-module ActiveRecord::ConnectionAdapters
-  TeradataAdapter = Class.new(AbstractAdapter) unless const_defined?(:TeradataAdapter)
-end
+require 'arjdbc/teradata/limit_helpers'
 
 module ::ArJdbc
   module Teradata
@@ -16,64 +14,6 @@ module ::ArJdbc
     def modify_types(tp)
       tp[:integer][:limit] = nil
       tp
-    end
-
-    # SQL SERVER CODE STARTS HERE
-
-    def add_limit_offset!(sql, options)
-      if options[:limit]
-        order = "ORDER BY #{options[:order] || determine_order_clause(sql)}"
-        sql.sub!(/ ORDER BY.*$/i, '')
-        replace_limit_offset!(sql, options[:limit], options[:offset], order)
-      end
-    end
-
-    def replace_limit_offset!(sql, limit, offset, order)
-      if limit
-        offset ||= 0
-        start_row = offset + 1
-        end_row = offset + limit.to_i
-        find_select = /\b(SELECT(?:\s+DISTINCT)?)\b(.*)/im
-        whole, select, rest_of_query = find_select.match(sql).to_a
-        rest_of_query.strip!
-        if rest_of_query[0] == "1"
-          rest_of_query[0] = "*"
-        end
-        if rest_of_query.chars.first == "*"
-          from_table = get_table_name(rest_of_query)
-          rest_of_query = from_table + '.' + rest_of_query
-        end
-        new_sql = "#{select} t.* FROM (SELECT ROW_NUMBER() OVER(#{order}) AS _row_num, #{rest_of_query}"
-        new_sql << ") AS t WHERE t._row_num BETWEEN #{start_row.to_s} AND #{end_row.to_s}"
-        sql.replace(new_sql)
-      end
-      sql
-    end
-
-    def get_table_name(sql)
-      if sql =~ /^\s*insert\s+into\s+([^\(\s,]+)\s*|^\s*update\s+([^\(\s,]+)\s*/i
-        $1
-      elsif sql =~ /\bfrom\s+([^\(\s,]+)\s*/i
-        $1
-      else
-        nil
-      end
-    end
-
-    def determine_order_clause(sql)
-      return $1 if sql =~ /ORDER BY (.*)$/
-      "1"
-      #table_name = get_table_name(sql)
-      #"#{table_name}.#{determine_primary_key(table_name)}"
-    end
-
-    def determine_primary_key(table_name)
-      primary_key = columns(table_name).detect { |column| column.primary || column.identity }
-      return primary_key.name if primary_key
-      # Look for an id column.  Return it, without changing case, to cover dbs with a case-sensitive collation.
-      columns(table_name).each { |column| return column.name if column.name =~ /^id$/i }
-      # Give up and provide something which is going to crash almost certainly
-      columns(table_name)[0].name
     end
 
   end
---
+++ lib/arjdbc/teradata/limit_helpers.rb
@@ -0,0 +1,98 @@
+module ArJdbc
+  module Teradata
+    module LimitHelpers
+
+      FIND_SELECT = /\b(SELECT(?:\s+DISTINCT)?)\b(.*)/im # :nodoc:
+
+      module TeradataReplaceLimitOffset
+
+        module_function
+
+        def replace_limit_offset!(sql, limit, offset, order)
+          if limit
+            offset ||= 0
+            start_row = offset + 1
+            end_row = offset + limit.to_i
+            _, select, rest_of_query = FIND_SELECT.match(sql).to_a
+            rest_of_query.strip!
+            if rest_of_query[0...1] == "1" && rest_of_query !~ /1 AS/i
+              rest_of_query[0] = "*"
+            end
+            if rest_of_query[0...1] == "*"
+              from_table = Utils.get_table_name(rest_of_query, true)
+              rest_of_query = from_table + '.' + rest_of_query
+            end
+            new_sql = "#{select} t.* FROM (SELECT ROW_NUMBER() OVER(#{order}) AS _row_num, #{rest_of_query}"
+            new_sql << ") AS t WHERE t._row_num BETWEEN #{start_row.to_s} AND #{end_row.to_s}"
+            sql.replace(new_sql)
+          end
+          sql
+        end
+
+        private
+
+        def get_table_name(sql)
+          if sql =~ /^\s*insert\s+into\s+([^\(\s,]+)\s*|^\s*update\s+([^\(\s,]+)\s*/i
+            $1
+          elsif sql =~ /\bfrom\s+([^\(\s,]+)\s*/i
+            $1
+          else
+            nil
+          end
+        end
+
+        def determine_order_clause(sql)
+          return $1 if sql =~ /ORDER BY (.*)$/
+          "1"
+          #table_name = get_table_name(sql)
+          #"#{table_name}.#{determine_primary_key(table_name)}"
+        end
+
+        def determine_primary_key(table_name)
+          primary_key = columns(table_name).detect { |column| column.primary || column.identity }
+          return primary_key.name if primary_key
+          # Look for an id column.  Return it, without changing case, to cover dbs with a case-sensitive collation.
+          columns(table_name).each { |column| return column.name if column.name =~ /^id$/i }
+          # Give up and provide something which is going to crash almost certainly
+          columns(table_name)[0].name
+        end
+
+      end
+
+      def replace_limit_offset!(sql, limit, offset, order)
+        if limit
+          offset ||= 0
+          start_row = offset + 1
+          end_row = offset + limit.to_i
+          find_select = /\b(SELECT(?:\s+DISTINCT)?)\b(.*)/im
+          whole, select, rest_of_query = find_select.match(sql).to_a
+          rest_of_query.strip!
+          if rest_of_query[0] == "1"
+            rest_of_query[0] = "*"
+          end
+          if rest_of_query.chars.first == "*"
+            from_table = get_table_name(rest_of_query)
+            rest_of_query = from_table + '.' + rest_of_query
+          end
+          new_sql = "#{select} t.* FROM (SELECT ROW_NUMBER() OVER(#{order}) AS _row_num, #{rest_of_query}"
+                                         new_sql << ") AS t WHERE t._row_num BETWEEN #{start_row.to_s} AND #{end_row.to_s}"
+          sql.replace(new_sql)
+        end
+        sql
+      end
+
+      module SqlServerAddLimitOffset
+
+        def add_limit_offset!(sql, options)
+          if options[:limit]
+            order = "ORDER BY #{options[:order] || determine_order_clause(sql)}"
+            sql.sub!(/ ORDER BY.*$/i, '')
+            replace_limit_offset!(sql, options[:limit], options[:offset], order)
+          end
+        end
+
+      end
+
+    end
+  end
+end
