4714ff16cb21672c29c796e8fd97ecd6e46162be
Lucas Maxwell <lucas@thecowsays.mu>
2013-05-22 15:59:49 +1000

    Make cop configurable

--- lib/arjdbc/teradata/connection_methods.rb
+++ lib/arjdbc/teradata/connection_methods.rb
@@ -2,7 +2,8 @@ class ActiveRecord::Base
   class << self
     def teradata_connection(config)
       config[:port] ||= 1025
-      config[:url] ||= "jdbc:teradata://#{config[:host]}/DATABASE=#{config[:database]},DBS_PORT=#{config[:port]},COP=OFF"
+      config[:cop] ||= "ON"
+      config[:url] ||= "jdbc:teradata://#{config[:host]}/DATABASE=#{config[:database]},DBS_PORT=#{config[:port]},COP=#{config[:cop]},CHARSET=UTF8"
       config[:driver] ||= "com.teradata.jdbc.TeraDriver"
       jdbc_connection(config)
     end
