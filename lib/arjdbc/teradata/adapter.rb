f25ace454b931d5330b27a39f8bacd43d2878e3b
Lucas Maxwell <lucas@thecowsays.mu>
2013-05-23 16:21:40 +1000

    [teradata] Move limit helper methods out to adapter

--- lib/arjdbc/teradata/adapter.rb
+++ lib/arjdbc/teradata/adapter.rb
@@ -22,6 +22,41 @@ module ::ArJdbc
       { 'teradata' => visitors, 'jdbcteradata' => visitors }
     end
 
+    def determine_order_clause(sql)
+      return $1 if sql =~ /ORDER BY (.*)$/
+      table_name = get_table_name(sql)
+      "#{table_name}.#{determine_primary_key(table_name)}"
+    end
+
+    def determine_primary_key(table_name)
+      primary_key = columns(table_name).detect { |column| column.primary }
+      return primary_key.name if primary_key
+      # Look for an id column and return it,
+      # without changing case, to cover DBs with a case-sensitive collation :
+      columns(table_name).each { |column| return column.name if column.name =~ /^id$/i }
+      # Give up and provide something which is going to crash almost certainly
+      columns(table_name)[0].name
+    end
+
+    GET_TABLE_NAME_INSERT_UPDATE_RE = /^\s*(INSERT|EXEC sp_executesql N'INSERT)\s+INTO\s+([^\(\s,]+)\s*|^\s*update\s+([^\(\s,]+)\s*/i
+
+    GET_TABLE_NAME_FROM_RE = /\bFROM\s+([^\(\)\s,]+)\s*/i
+
+    def get_table_name(sql, qualified = nil)
+      if sql =~ GET_TABLE_NAME_INSERT_UPDATE_RE
+        tn = $2 || $3
+        qualified ? tn : unqualify_table_name(tn)
+      elsif sql =~ GET_TABLE_NAME_FROM_RE
+        qualified ? $1 : unqualify_table_name($1)
+      else
+        nil
+      end
+    end
+
+    def unqualify_table_name(table_name)
+      table_name.to_s.split('.').last
+    end
+
   end
 end
 
